29d9d7c3d73d1e5784ef1ac526af61f8db90ac6b
LAJOS010 <91637290+LAJOS010@users.noreply.github.com>
2021-12-12 23:32:28 +0100

    Update conv_training_on_batch.py

--- conv/conv_training_on_batch.py
+++ conv/conv_training_on_batch.py
@@ -4,18 +4,26 @@ import os
 from  conv_model import conv_model
 import numpy as np
 from os import path
+import argparse
 from PIL import Image
 from tensorflow import keras
 from keras.preprocessing.image import img_to_array
+from keras.models import save, load_model
 #from keras.callbacks import EarlyStopping
 
 #===============================INITIALIZING=================================
-
-epochs = 60
+parser = argparse.ArgumentParser()
+parser.add_argument("--load", default=True)
+args = parser.parse_args()
+epochs = 30
 batch_size = 512
 patience = 15
-val_split = 5 # menet
-model = conv_model()
+val_split = 30 # menet
+pathmod = 'datas/'
+if args.load:
+	model=load_model(pathmod + 'reinf_learning_model')
+else:
+	model = conv_model()
 
 dir = 'datas/'
 data_codes = np.load(dir+'names.npy')
@@ -27,14 +35,16 @@ os.system('clear')
 for epoch in range(epochs):
   print("Epoch:"+str(epoch+1)+"/"+str(epochs))
   name = dir + 'names'
-  run_num = 175 #len(data_codes)
+  run_num = 100 #len(data_codes)
   randname = str(data_codes[run_num])
 
 #================================TRAINING====================================
   count = 1
   losses = []
   accs = []
-  while (run_num <= len(data_codes)-1):	
+  #to=len(data_codes)-1
+  to=150
+  while (run_num <= to):	
     x_train = []
     y_train = []
     found=0
@@ -61,7 +71,7 @@ for epoch in range(epochs):
       else:
         run_num += 1
         
-        if run_num > len(data_codes)-1: break
+        if run_num > to: break
         randname = str(data_codes[run_num])
         count = 1
         namex = dir + 'x/' + randname + '_' + str(count) + '.jpg'
@@ -83,11 +93,11 @@ for epoch in range(epochs):
 
     y_train = keras.utils.to_categorical(y_train,5)
     x_train = np.array(x_train).reshape(found,40,80,3)
-    if run_num < len(data_codes)-val_split:  # train
+    if run_num < to+1-val_split:  # train
 
       model.train_on_batch(x_train, y_train)
       print("trained runs:   "+str(run_num),end="\r")
-    elif (run_num < len(data_codes)-1): # validation
+    elif (run_num < to): # validation
       loss,acc = model.evaluate(x_train, y_train,batch_size=batch_size)
       losses.append(loss)
       accs.append(acc)
@@ -97,15 +107,17 @@ for epoch in range(epochs):
   accs=np.array(accs)
   loss = losses.mean()
   acc = accs.mean()
-  print("validated       loss=", str(loss), "   accuracy=", str(acc), "   patience=", str(pat_count), end="\r")
-  if acc > best_acc:
+  if loss < best_loss and acc > best_acc:
      best_acc = acc
+     best_loss = loss
      pat_count = 0
+     print("validated       loss=", str(loss), "   accuracy=", str(acc), "   patience=", str(pat_count), end="\r")
      print("\n----------------------------------saving-----------------------------\n")
-     model.save(dir + 'reinf_learning_model')
+     model.save(dir + 'CONV_model')
      print("\n----------------------------------saved------------------------------\n")
   else:
      pat_count += 1
+     print("validated       loss=", str(loss), "   accuracy=", str(acc), "   patience=", str(pat_count), end="\r")
    
   if(pat_count >= patience):
      print("EARLY STOPPING      loss:", str(best_loss), "   accuracy=", str(acc), end="\n")
