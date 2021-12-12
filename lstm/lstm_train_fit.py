d04dc4f9df67577d5be3e1a4ea9397d532638a95
zsoca000 <91702285+zsoca000@users.noreply.github.com>
2021-12-12 18:25:12 +0100

    own mini batch training

--- lstm/lstm_train_fit.py
+++ lstm/lstm_train_fit.py
@@ -1,4 +1,3 @@
-
 #===============================DEPNDENCIES==================================
 
 from  lstm_model import lstm_model
@@ -16,30 +15,33 @@ batch_size = 128
 model, time_steps = lstm_model()
 
 dir = 'datas/'
-data_codes = np.load(dir+'names.npy')
+data_codes = np.load(dir+'names.npy') # the ids of runs
 
 #===============================DATA_READING=================================
 
 
-b_s = 1
+b_s = 1 # the actula size of batch
 
 x_train = []
 y_train = []
 
+n = 1 # the number of loaded datas
 
+# iterating trough all datas
 for n in data_codes:
    i = 1
    name_x = ""
    name_y = ""
    while (path.isfile(name_x) and path.isfile(name_y)) or i==1:
      
-     name_x = dir + 'x/' + str(n) + '_' + str(i) + '.jpg'
-     name_y = dir + 'y/' + str(n) + '_' + str(i) + '.npy'
+     name_x = dir + 'x/' + str(n) + '_' + str(i) + '.jpg'  # images
+     name_y = dir + 'y/' + str(n) + '_' + str(i) + '.npy'  # keyboard commands
      
      if path.isfile(name_x) and path.isfile(name_y):
        
-       if b_s == 256:
+       if b_s == batch_size + 1:
           
+          # convert the datas to be acceptable for the lstm model
           tmp_x = []
           for j in range(len(x_train)-5):
               tmp_x.append([x_train[j],x_train[j+1],x_train[j+2],x_train[j+3],x_train[j+4]])
@@ -50,60 +52,28 @@ for n in data_codes:
           x_train = np.array(x_train).reshape(len(x_train),time_steps,40,80,3)
           y_train = keras.utils.to_categorical(y_train,5)
           
-          print('batch learn start')
-          model.train_on_batch(x_train, y_train)
-          print('batch learn end')
+          model.train_on_batch(x_train, y_train) # train the model
+          
+          # refresh variables
           x_train = []
           y_train = []
           b_s = 1
-       
-       else:
+
+       # load datas
+       img = Image.open(name_x)
+       x = img_to_array(img)
+       x /= 255
+       x_train.append(x)
+
+       y = np.load(name_y)
+       y= np.argmax(y)
+       y_train.append(y)
+       b_s += 1
+         
           
-          img = Image.open(name_x)
-          x = img_to_array(img)
-          x /= 255
-          x_train.append(x)
-
-          y = np.load(name_y)
-          y= np.argmax(y)
-          y_train.append(y)
-          b_s += 1
-     
-     i+=1
+       print('load( '+str(n)+' ): ' + name_x, end='\r')
+       n+=1  
+       i+=1
 
      
-
-print(len(x_train))
-
-#=============================TIMESTEP_FORM=====================================
-
-tmp_x = []
-for j in range(len(x_train)-5):
-  tmp_x.append([x_train[j],x_train[j+1],x_train[j+2],x_train[j+3],x_train[j+4]])
-
-x_train = tmp_x
-
-
-y_train = y_train[5:]
-
-#================================TRAINING=======================================
-
-
-
-x_train = np.array(x_train).reshape(len(x_train),time_steps,40,80,3)
-y_train = keras.utils.to_categorical(y_train,5)
-
-"""
-early_stopping = EarlyStopping(monitor='val_loss', patience=8)
-model.fit(x_train,
-          y_train,
-          epochs=epochs,
-          validation_split = 0.2,
-          batch_size=batch_size,  
-          callbacks = early_stopping,        
-          verbose=1,
-          shuffle=True)
-"""    
-model.save(dir + 'reinf_learning_model')
-
-
+model.save(dir + 'reinf_learning_model') # save the model
